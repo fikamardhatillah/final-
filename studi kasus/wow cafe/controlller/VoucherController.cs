6d02ead1e4fc3153c504a25c136081633df5dac4
fikamardhatillah <61910327+fikamardhatillah@users.noreply.github.com>
2021-01-12 09:04:08 +0700

    Add files via upload

---
+++ studi kasus/wow cafe/controlller/VoucherController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cafe.Model;
+
+namespace Cafe.Controller
+{
+    class VoucherController
+    {
+        public List<Diskon> diskon;
+
+        public VoucherController()
+        {
+            diskon = new List<Diskon>();
+        }
+
+        public void addPromo(Diskon diskon)
+        {
+            this.diskon.Add(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.diskon;
+        }
+    }
+}
