fecf24a1a51635c477ae6a741eb0079bce51e33b
fikamardhatillah <61910327+fikamardhatillah@users.noreply.github.com>
2021-01-12 09:04:48 +0700

    Add files via upload

---
+++ studi kasus/wow cafe/modelll/Bayar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Model
+{
+    class Bayar
+    {
+        OnPaymentChangedListener paymentListener;
+        public Bayar(OnPaymentChangedListener paymentListener)
+        {
+
+            this.paymentListener = paymentListener;
+        }
+
+        public void updateTotal(double subTotal, double promo)
+        {
+
+            double total = subTotal - promo;
+            this.paymentListener.onPriceUpdated(subTotal, total, promo);
+
+        }
+    }
+
+    interface OnPaymentChangedListener
+    {
+        void onPriceUpdated(double subtTotal, double total, double promo);
+
+    }
+}
+
+
---
+++ studi kasus/wow cafe/modelll/Diskon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Model
+{
+
+    public class Diskon
+    {
+        public string diskon { get; set; }
+        public double potongan { get; set; }
+
+        public Diskon(string diskon, double potongan)
+        {
+            this.diskon = diskon;
+            this.potongan = potongan;
+        }
+
+
+    }
+}
